5bd75865c670a76c696e7e3befbe3108dcf3b961
nexumi-ai <menardrush@gmail.com>
2022-06-20 10:32:19 +0800

    add 17 places with categories and coordinates

---
+++ src/data/places.js
@@ -0,0 +1,21 @@
+export const places = [
+  { id: 1, name: 'Maasin Cathedral', category: 'Landmark', icon: 'church', desc: 'Historic Roman Catholic cathedral built in 1770 with Spanish colonial architecture.', lat: 10.1339, lng: 124.8442, rating: 4.8, tags: ['religious', 'historic', 'architecture'] },
+  { id: 2, name: 'Monte Cueva', category: 'Nature', icon: 'mountain', desc: 'Hilltop shrine with panoramic views of the city and Bohol Sea.', lat: 10.1352, lng: 124.8398, rating: 4.7, tags: ['religious', 'hiking', 'scenic'] },
+  { id: 3, name: 'Maasin City Hall', category: 'Government', icon: 'building', desc: 'Seat of local government serving the citizens of Maasin.', lat: 10.1341, lng: 124.8461, rating: 4.0, tags: ['government', 'services'] },
+  { id: 4, name: 'Agas-Agas Bridge', category: 'Landmark', icon: 'bridge', desc: 'The tallest bridge in the Philippines at 89 meters spanning the Sogod Bay ravine.', lat: 10.3714, lng: 124.9613, rating: 4.9, tags: ['landmark', 'adventure', 'scenic'] },
+  { id: 5, name: 'Guinsohotan Falls', category: 'Nature', icon: 'water', desc: 'Two-tiered waterfall surrounded by lush tropical forest.', lat: 10.1654, lng: 124.8125, rating: 4.6, tags: ['waterfall', 'swimming', 'nature'] },
+  { id: 6, name: 'Maasin Public Market', category: 'Commerce', icon: 'market', desc: 'Vibrant marketplace with fresh seafood and local delicacies.', lat: 10.1335, lng: 124.8451, rating: 4.2, tags: ['shopping', 'food', 'local'] },
+  { id: 7, name: 'Maasin Port', category: 'Transport', icon: 'port', desc: 'Ferry terminal with routes to Cebu and neighboring islands.', lat: 10.1314, lng: 124.8410, rating: 4.1, tags: ['transport', 'ferry', 'travel'] },
+  { id: 8, name: 'Southern Leyte State U', category: 'Education', icon: 'school', desc: 'Higher education institution offering degree programs in IT and engineering.', lat: 10.1297, lng: 124.8386, rating: 4.3, tags: ['education', 'campus'] },
+  { id: 9, name: 'Bato Beach Resort', category: 'Leisure', icon: 'beach', desc: 'White sand beach with cottages and water sports.', lat: 10.1211, lng: 124.8324, rating: 4.5, tags: ['beach', 'resort', 'swimming'] },
+  { id: 10, name: 'Kuting Reef', category: 'Nature', icon: 'reef', desc: 'Protected marine sanctuary with vibrant coral reefs ideal for snorkeling.', lat: 10.1198, lng: 124.8299, rating: 4.8, tags: ['diving', 'marine', 'snorkeling'] },
+  { id: 11, name: 'Maasin River', category: 'Nature', icon: 'river', desc: 'Scenic river popular for river cruises and fishing.', lat: 10.1327, lng: 124.8473, rating: 4.0, tags: ['river', 'fishing', 'cruise'] },
+  { id: 12, name: 'Abiera Museum', category: 'Culture', icon: 'museum', desc: 'Local museum showcasing Maasin heritage with artifacts.', lat: 10.1333, lng: 124.8459, rating: 4.4, tags: ['museum', 'history', 'culture'] },
+  { id: 13, name: 'Saint Joseph College', category: 'Education', icon: 'school', desc: 'Private Catholic institution offering quality education.', lat: 10.1356, lng: 124.8433, rating: 4.6, tags: ['education', 'catholic', 'college'] },
+  { id: 14, name: 'Maasin Doctors Hospital', category: 'Services', icon: 'hospital', desc: 'Primary healthcare facility serving the city.', lat: 10.1324, lng: 124.8466, rating: 4.2, tags: ['healthcare', 'hospital'] },
+  { id: 15, name: 'Lungsodaan Cave', category: 'Nature', icon: 'cave', desc: 'Limestone cave system with stalactite formations.', lat: 10.1512, lng: 124.8301, rating: 4.3, tags: ['cave', 'adventure'] },
+  { id: 16, name: 'Maasin City Plaza', category: 'Leisure', icon: 'park', desc: 'Central public plaza with gardens and playground.', lat: 10.1338, lng: 124.8450, rating: 4.1, tags: ['park', 'recreation', 'family'] },
+  { id: 17, name: 'Mantahan Festival', category: 'Culture', icon: 'festival', desc: 'Annual city festival with street dancing and cultural shows.', lat: 10.1340, lng: 124.8448, rating: 4.7, tags: ['festival', 'culture', 'annual'] },
+];
+
+export const categories = ['Landmark', 'Nature', 'Culture', 'Education', 'Leisure', 'Commerce', 'Transport', 'Services', 'Government'];
